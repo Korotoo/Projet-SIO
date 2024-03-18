02de793e21f43aa987ad49b7c9e96d6259b804dc
.Korotoo <nathanrosso583@gmail.com>
2024-03-18 16:45:53 +0100

    maj

---
+++ src/main/java/fr/pgah/Controllers/EcranEmploye.java
@@ -0,0 +1,19 @@
+package fr.pgah.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class EcranEmploye {
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField matricule;
+
+    @FXML
+    private TextField nom;
+
+}
+
+    
+}
